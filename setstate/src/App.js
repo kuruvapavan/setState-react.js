8303d223945b9a91ab180cb61011a1ffda9033d4
kuruvapavan <94837551+kuruvapavan@users.noreply.github.com>
2021-12-28 22:05:12 +0530

    up

---
+++ setstate/src/App.js
@@ -0,0 +1,37 @@
+
+import React, { Component } from 'react'
+	
+class App extends Component {
+constructor(props){
+	super(props)
+		
+	// Set initial state
+	this.state = {greeting :
+		'Click the button to receive greetings'}
+		
+	// Binding this keyword
+	this.updateState = this.updateState.bind(this)
+}
+	
+updateState(){
+	// Changing state
+	this.setState({greeting :
+				'welcome to my world !!'})
+}
+	
+render(){
+	return (
+	<div>
+	<h2>Greetings Portal</h2>
+	<p>{this.state.greeting}</p>
+	
+		{/* Set click handler */}
+		<button onClick={this.updateState}>
+		Click me!
+		</button>
+	</div>
+	)
+}
+}
+	
+export default App;
